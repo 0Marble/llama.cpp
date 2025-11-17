329ec98015c7c5b49b05e59a328021a426611db1
姜秀珍 <2501213377@cninfer02.localdomain>
2025-11-17 19:14:25 +0800

    second try : failed

--- ggml/src/ggml-cann/aclnn_ops.h
+++ ggml/src/ggml-cann/aclnn_ops.h
@@ -1165,6 +1165,7 @@ void ggml_cann_op_unary_gated(std::function<void(ggml_backend_cann_context &, ac
 
 void ggml_cann_ssm_conv(ggml_backend_cann_context & ctx, ggml_tensor * dst);
 
+
 /**
  * @brief Helper macro to call a unary ACL operator via ggml_cann_op_unary.
  *

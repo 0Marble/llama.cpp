2037b379b5213cc47256097427764f4590c25fc9
Aleksei Lobanov <2401213370@stu.pku.edu.cn>
2025-11-20 14:30:42 +0800

    added empty impl for ssm_conv

--- ggml/src/ggml-cann/ggml-cann.cpp
+++ ggml/src/ggml-cann/ggml-cann.cpp
@@ -1839,6 +1839,9 @@ static bool ggml_cann_compute_forward(ggml_backend_cann_context & ctx, struct gg
         case GGML_OP_FLASH_ATTN_EXT:
             ggml_cann_flash_attn_ext(ctx, dst);
             break;
+        case GGML_OP_SSM_CONV:
+            ggml_cann_ssm_conv(ctx, dst);
+            break;
         default:
             return false;
     }
@@ -2475,6 +2478,8 @@ static bool ggml_backend_cann_supports_op(ggml_backend_dev_t dev, const ggml_ten
                 }
                 return true;
             }
+        case GGML_OP_SSM_CONV:
+            return true;
         default:
             return false;
     }
